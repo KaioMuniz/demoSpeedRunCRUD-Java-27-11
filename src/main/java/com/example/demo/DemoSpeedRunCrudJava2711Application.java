9ddc0557642472999b9045e4ffc9cdbaec8e17e2
KaioMuniz <kkaioribeiro@gmail.com>
2025-11-27 11:37:04 -0300

    projeto iniciado com spring web e spring dev tools

---
+++ src/main/java/com/example/demo/DemoSpeedRunCrudJava2711Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpeedRunCrudJava2711Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpeedRunCrudJava2711Application.class, args);
+	}
+
+}
