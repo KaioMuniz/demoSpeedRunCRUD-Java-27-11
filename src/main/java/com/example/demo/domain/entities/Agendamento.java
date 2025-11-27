facd86206b23aa1c5e80f407a2eebb19557b3fd4
KaioMuniz <kkaioribeiro@gmail.com>
2025-11-27 11:41:13 -0300

    desenho base da entidade

---
+++ src/main/java/com/example/demo/domain/entities/Agendamento.java
@@ -0,0 +1,16 @@
+package com.example.demo.domain.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Agendamento {
+	
+	private UUID id;
+	private LocalDateTime entrada;
+	private LocalDateTime saida;
+	private String modoPagamento;
+
+}
