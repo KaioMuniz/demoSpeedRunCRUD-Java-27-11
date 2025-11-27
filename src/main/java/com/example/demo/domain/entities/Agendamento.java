d2ec4517b22fdb8f21b515ab592bdac832cf231b
KaioMuniz <kkaioribeiro@gmail.com>
2025-11-27 12:01:37 -0300

    montei as colunas no banco de dados por meio da jpa; montei um banco postgres e configurei no aplication properties

--- src/main/java/com/example/demo/domain/entities/Agendamento.java
+++ src/main/java/com/example/demo/domain/entities/Agendamento.java
@@ -3,14 +3,36 @@ package com.example.demo.domain.entities;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="Agendamento")
 public class Agendamento {
 	
+	
+	@Column(name="id")
+	@Id
+	@GeneratedValue(strategy=GenerationType.UUID)
 	private UUID id;
+	
+	@Column(name="entrada", nullable=true)
 	private LocalDateTime entrada;
+	
+	@Column(name="saida", nullable=true)
 	private LocalDateTime saida;
+	
+	@Column(name="modoPagamento", nullable=true)
 	private String modoPagamento;
 
 }
