92ee2852893e5b4214b9d47535a0bc30a2925195
KaioMuniz <kkaioribeiro@gmail.com>
2025-11-27 12:36:37 -0300

    montei todo o controller

--- src/main/java/com/example/demo/controllers/AgendamentoController.java
+++ src/main/java/com/example/demo/controllers/AgendamentoController.java
@@ -1,33 +1,57 @@
 package com.example.demo.controllers;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.dtos.in.AtualizarAgendamentoPorIdRequest;
+import com.example.demo.domain.dtos.in.CriarAgendamentoRequest;
+import com.example.demo.domain.dtos.out.AtualizarAgendamentoPorIdResponse;
+import com.example.demo.domain.dtos.out.ConsultarAgendamentoPorIdResponse;
+import com.example.demo.domain.dtos.out.CriarAgendamentoResponse;
+import com.example.demo.domain.dtos.out.DeletarAgendamentoPorIdResponse;
+import com.example.demo.domain.services.AgendamentoService;
 
 @RestController
 @RequestMapping("/api/v1/agenda")
 public class AgendamentoController {
 	
+	@Autowired AgendamentoService agendamentoService;
+	
 	@GetMapping
-	public void consultarAgendamentoPorId() {
+	public ResponseEntity<ConsultarAgendamentoPorIdResponse> consultarAgendamentoPorId(@RequestParam UUID id) {
 		
+		
+		return ResponseEntity.ok(agendamentoService.get(id));
 	}
 	
 	@PostMapping
-	public void criarAgendamento() {
+	public ResponseEntity<CriarAgendamentoResponse> criarAgendamento(@RequestBody CriarAgendamentoRequest request ) {
 		
+		
+		return ResponseEntity.ok(agendamentoService.post(request));
 	}
 	
 	@PutMapping
-	public void atualizarAgendamentoPorId() {
+	public ResponseEntity<AtualizarAgendamentoPorIdResponse> atualizarAgendamentoPorId(@RequestParam UUID id,@RequestBody AtualizarAgendamentoPorIdRequest request ) {
 		
+		
+		return ResponseEntity.ok(agendamentoService.put(id, request));
 	}
 	
 	@DeleteMapping
-	public void deletarAgendamentoPorId() {
+	public ResponseEntity<DeletarAgendamentoPorIdResponse> deletarAgendamentoPorId(@RequestParam UUID id) {
+		
+		return ResponseEntity.ok(agendamentoService.delete(id));
 		
 	}
 	
--- src/main/java/com/example/demo/domain/services/AgendamentoService.java
+++ src/main/java/com/example/demo/domain/services/AgendamentoService.java
@@ -1,25 +1,38 @@
 package com.example.demo.domain.services;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.dtos.in.AtualizarAgendamentoPorIdRequest;
+import com.example.demo.domain.dtos.in.ConsultarAgendamentoPorIdRequest;
+import com.example.demo.domain.dtos.in.CriarAgendamentoRequest;
+import com.example.demo.domain.dtos.out.AtualizarAgendamentoPorIdResponse;
+import com.example.demo.domain.dtos.out.ConsultarAgendamentoPorIdResponse;
+import com.example.demo.domain.dtos.out.CriarAgendamentoResponse;
+import com.example.demo.domain.dtos.out.DeletarAgendamentoPorIdResponse;
 
 @Service
 public class AgendamentoService {
+	
+	@Autowired AgendamentoService agendamentoService;
 
-	
-	
-	public void get() {
-		
+	public ConsultarAgendamentoPorIdResponse get(UUID id) {
+
+		return null;
 	}
-	
-	public void post() {
-		
+
+	public CriarAgendamentoResponse post(CriarAgendamentoRequest request) {
+
+		return null;
 	}
-	
-	public void put() {
-		
+
+	public AtualizarAgendamentoPorIdResponse put(UUID id, AtualizarAgendamentoPorIdRequest request) {
+		return null;
 	}
-	
-	public void delete() {
-		
+
+	public DeletarAgendamentoPorIdResponse delete(UUID id) {
+		return null;
 	}
 }
