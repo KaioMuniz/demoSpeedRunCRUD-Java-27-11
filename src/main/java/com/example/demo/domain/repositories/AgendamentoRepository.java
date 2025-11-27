d188cdbc8873ff7c6e140c2fd67f8763ddb912b3
KaioMuniz <kkaioribeiro@gmail.com>
2025-11-27 12:14:13 -0300

    base da service

--- src/main/java/com/example/demo/domain/repositories/AgendamentoRepository.java
+++ src/main/java/com/example/demo/domain/repositories/AgendamentoRepository.java
@@ -3,9 +3,11 @@ package com.example.demo.domain.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.entities.Agendamento;
 
+@Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, UUID> {
 
 }
---
+++ src/main/java/com/example/demo/domain/services/AgendamentoService.java
@@ -0,0 +1,25 @@
+package com.example.demo.domain.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendamentoService {
+
+	
+	
+	public void get() {
+		
+	}
+	
+	public void post() {
+		
+	}
+	
+	public void put() {
+		
+	}
+	
+	public void delete() {
+		
+	}
+}
