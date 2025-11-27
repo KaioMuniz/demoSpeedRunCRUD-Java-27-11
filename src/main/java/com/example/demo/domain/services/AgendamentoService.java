2fa7f787fdc2252d0e6cc61a10a43528074f080a
KaioMuniz <kkaioribeiro@gmail.com>
2025-11-27 12:49:48 -0300

    service finalizada; crud finalizado horaTotal : 1:15:40

--- src/main/java/com/example/demo/domain/services/AgendamentoService.java
+++ src/main/java/com/example/demo/domain/services/AgendamentoService.java
@@ -6,33 +6,80 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.dtos.in.AtualizarAgendamentoPorIdRequest;
-import com.example.demo.domain.dtos.in.ConsultarAgendamentoPorIdRequest;
 import com.example.demo.domain.dtos.in.CriarAgendamentoRequest;
 import com.example.demo.domain.dtos.out.AtualizarAgendamentoPorIdResponse;
 import com.example.demo.domain.dtos.out.ConsultarAgendamentoPorIdResponse;
 import com.example.demo.domain.dtos.out.CriarAgendamentoResponse;
 import com.example.demo.domain.dtos.out.DeletarAgendamentoPorIdResponse;
+import com.example.demo.domain.entities.Agendamento;
+import com.example.demo.domain.repositories.AgendamentoRepository;
 
 @Service
 public class AgendamentoService {
 	
-	@Autowired AgendamentoService agendamentoService;
+	@Autowired AgendamentoRepository agendamentoRepository;
 
 	public ConsultarAgendamentoPorIdResponse get(UUID id) {
 
-		return null;
+		var agenda = agendamentoRepository.findById(id).orElseThrow(() -> new RuntimeException());
+		
+		
+		
+		return new ConsultarAgendamentoPorIdResponse(
+				agenda.getEntrada(),
+				agenda.getSaida(),
+				agenda.getModoPagamento());
 	}
 
 	public CriarAgendamentoResponse post(CriarAgendamentoRequest request) {
 
-		return null;
+		var agenda = new Agendamento();
+		
+		agenda.setEntrada(agenda.getEntrada());
+		agenda.setSaida(agenda.getSaida());
+		agenda.setModoPagamento(agenda.getModoPagamento());
+		
+		agendamentoRepository.save(agenda);
+		
+		
+		
+		return new CriarAgendamentoResponse(
+				agenda.getEntrada(),
+				agenda.getSaida(),
+				agenda.getModoPagamento());
+		
+		
 	}
 
 	public AtualizarAgendamentoPorIdResponse put(UUID id, AtualizarAgendamentoPorIdRequest request) {
-		return null;
+		
+		var agenda = agendamentoRepository.findById(id).orElseThrow(() -> new RuntimeException());
+		
+		
+		agenda.setEntrada(agenda.getEntrada());
+		agenda.setSaida(agenda.getSaida());
+		agenda.setModoPagamento(agenda.getModoPagamento());
+		
+		agendamentoRepository.save(agenda);
+		
+		
+		
+		return new AtualizarAgendamentoPorIdResponse(
+				agenda.getEntrada(),
+				agenda.getSaida(),
+				agenda.getModoPagamento());
 	}
 
 	public DeletarAgendamentoPorIdResponse delete(UUID id) {
-		return null;
+		
+		var agenda = agendamentoRepository.findById(id).orElseThrow(() -> new RuntimeException());
+		
+		agendamentoRepository.findById(id);
+		
+		return new DeletarAgendamentoPorIdResponse(
+				agenda.getEntrada(),
+				agenda.getSaida(),
+				agenda.getModoPagamento()
+				);
 	}
 }
